4fb338e7e3ceaf51d2451a49525152cbddecf1cd
avikamalik <70952746+avikamalik@users.noreply.github.com>
2022-01-27 16:46:21 +0530

    Update performance.py

--- performance.py
+++ performance.py
@@ -3,22 +3,17 @@ import pandas as pd
 import csv
 import statistics
 import random
-#reading scores data
 df = pd.read_csv("StudentsPerformance.csv")
 data = df["reading score"].tolist()
-#Calculating the mean and the standard deviation
 mean = sum(data) / len(data)
 std_deviation = statistics.stdev(data)
 median = statistics.median(data)
 mode = statistics.mode(data)
-#Finding 1 standard deviation stard and end values, and 2 standard deviations stard and end values
 first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
 second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
 third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
-#Plotting the chart
 fig = ff.create_distplot([data], ["reading scores"], show_hist=False)
 fig.show()
-#Printing the findings
 list_of_data_within_1_std_deviation = [result for result in data if result > first_std_deviation_start and result < first_std_deviation_end]
 list_of_data_within_2_std_deviation = [result for result in data if result > second_std_deviation_start and result < second_std_deviation_end]
 list_of_data_within_3_std_deviation = [result for result in data if result > third_std_deviation_start and result < third_std_deviation_end]
